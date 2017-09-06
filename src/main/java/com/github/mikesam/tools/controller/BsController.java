76e1af2e815aec05043f105a290c8e93a21a5529
gituser1 <ceo@mcylm.com>
2017-09-06 11:01:43 +0800

    daadasadasda

---
+++ src/main/java/com/github/mikesam/tools/controller/BsController.java
@@ -0,0 +1,10 @@
+package com.github.mikesam.tools.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Mikesam on 2017/9/6.
+ */
+@Controller
+public class BsController {
+}
